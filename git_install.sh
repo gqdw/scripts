e54e8cbdaf3f83da13cff68f8dd35b4f92d1eea9
aca <aca_jingru@qq.com>
2014-10-14 22:58:16 +0800

    modified:   git_install.sh

--- git_install.sh
+++ git_install.sh
@@ -5,7 +5,9 @@ cd /opt
 wget  http://zy-res.oss-cn-hangzhou.aliyuncs.com/git/v2.1.2.tar.gz
 tar xf v2.1.2.tar.gz
 cd git-2.1.2/
-yum install -y autoconf gcc zlib-devel gettext  perl-ExtUtils-MakeMaker
+yum install -y autoconf gcc zlib-devel gettext  perl-ExtUtils-MakeMaker openssl-devel curl-devel expat-devel
+make configure
+./configure --prefix=/usr
 make -j4
 make install
 git --version
