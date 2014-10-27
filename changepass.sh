956aa236bdae9bb2475c69554a1c847e61c3ebfd
aca <aca_jingru@qq.com>
2014-10-27 14:08:38 +0800

    modified:   changepass.sh 	modified:   nginx_install.sh

--- changepass.sh
+++ changepass.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if ! grep -q danqoo /etc/passwd
-then
-	useradd -m -s /bin/bash danqoo
+#if ! grep -q danqoo /etc/passwd
+#then
+#	useradd -m -s /bin/bash danqoo
+#fi
+#usermod -G sshers danqoo
+if [ $# -ne 1 ];then
+	len=10
+else
+	len=$1
 fi
-usermod -G sshers danqoo
-PASS=`cat /dev/urandom |tr -cd '0-9a-z'|head -c 10`
+PASS=`cat /dev/urandom |tr -cd '0-9a-z'|head -c $len`
 hostname
-echo "danqoo:$PASS"| chpasswd 
+#echo "danqoo:$PASS"| chpasswd 
 echo "$PASS"
--- nginx_install.sh
+++ nginx_install.sh
@@ -4,6 +4,6 @@ tar xf nginx-1.6.0.tar.gz
 cd nginx-1.6.0
 yum install -y  pcre-devel zlib-devel
 ./configure --prefix=/alidata/nginx
-make -j4
+make -j
 make install
 
