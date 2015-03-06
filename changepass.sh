ed1b1d1785bce5c42855dc5b86048e2503538389
aca <aca_jingru@qq.com>
2015-03-06 12:59:56 +0800

    ..

--- changepass.sh
+++ changepass.sh
@@ -5,12 +5,19 @@
 #	useradd -m -s /bin/bash danqoo
 #fi
 #usermod -G sshers danqoo
-if [ $# -ne 1 ];then
-	len=10
-else
-	len=$1
-fi
-PASS=`cat /dev/urandom |tr -cd '0-9a-zA-Z'|head -c $len`
-#hostname
-#echo "danqoo:$PASS"| chpasswd 
-echo "$PASS"
+get_pass()
+{
+	if [ $# -ne 1 ];then
+		len=10
+	else
+		len=$1
+	fi
+	PASS=`cat /dev/urandom |tr -cd '0-9a-zA-Z'|head -c $len`
+	#hostname
+	#echo "danqoo:$PASS"| chpasswd 
+	echo "$PASS"
+}
+#p=$(get_pass)
+p=`get_pass`
+echo "$p"
+#echo "$p"
