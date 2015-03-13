5e432a106a8b642ddeec46cd1238d8aa499a2a38
aca <aca_jingru@qq.com>
2015-03-13 14:31:58 +0800

    xml test file

---
+++ xmltest.py
@@ -0,0 +1,8 @@
+xml1='''<?xml version="1.0" encoding="utf-8" ?><returnsms>
+ <returnstatus>Success</returnstatus>
+ <message>ok</message>
+ <remainpoint>9997</remainpoint>
+ <taskID>7236754</taskID>
+ <successCounts>1</successCounts></returnsms>'''
+#import xml.etree.ElementTree as ET
+#tree = ET.parse(xml1)
