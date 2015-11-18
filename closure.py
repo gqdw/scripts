faf3f14fab77b27311218baf6fe43efda261d700
gqdw <aca_jingru@qq.com>
2015-11-18 13:48:05 +0800

    closure function test

---
+++ closure.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+closure function test
+"""
+
+
+def maker(N):
+	def action(X):
+		return X ** N
+	return action
+
+f = maker(3)
+print f(3), f(4)
+g = maker(2)
+print g(3), g(4)
