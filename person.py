2035920971800b8eb84ddb9820b943ecd0cc503f
gqdw <aca_jingru@qq.com>
2015-11-21 19:38:29 +0800

    person

---
+++ person.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+for test class 
+"""
+
+class Person:
+	def __init__(self, name, job=None, pay=0):
+		self.name = name
+		self.job = job
+		self.pay = pay
+
+
+if __name__ == '__main__':
+	bob = Person('bob smith')
+	sue = Person('sue jones', job='dev', pay=100000)
+	print bob.name, bob.pay
+	print sue.name, sue.pay
