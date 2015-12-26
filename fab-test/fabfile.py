229869fab22f485d5e39f948bd556067321a25ea
gqdw <aca_jingru@qq.com>
2015-12-26 18:46:21 +0800

    add fabfile.py online ,to deploy frontend dist dir

--- fab-test/fabfile.py
+++ fab-test/fabfile.py
@@ -1,12 +1,28 @@
 from fabric.api import *
 
-env.user = 'root'
-env.hosts = ['123.56.229.114']
+env.user = 'deploy'
+env.hosts = ['192.168.100.52']
 
 def test1():
-	run('ls -l /tmp')
+	print "test1"
+	output = local('ls -l /tmp', capture=True)
+	print "output: -->" + output
 
 def test2():
 	with cd('/usr/local'):
 		run('pwd')
 		run('ls -l')
+
+def deploy():
+	local('tar cf dist.tar dist')
+	d = local('pwd', capture=True)
+	# print type(d)
+	filename = "%s/dist.tar" % d
+	dirname = "%s/dist" % d
+	print filename
+	put(filename, filename)
+	with cd(d):
+		run('rm -rf dist')
+		run('tar xf dist.tar')
+		run('ls -l')
+
