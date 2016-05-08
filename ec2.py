e7d8ed396facd52de24e352c774cb77895bb521f
gqdw <aca_jingru@qq.com>
2016-05-08 10:21:35 +0800

    es.count: count the docs

--- ec2.py
+++ ec2.py
@@ -4,12 +4,18 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 
 # 10.168.105.153:9200
-es = Elasticsearch(['http://10.168.105.153'])
 # print es.info()
 # print es.indices
 # create index test-0508
-es.indices.create(index='test-0508', ignore=400)
+# es.indices.create(index='test-0508', ignore=400)
 # es.indices.delete('test-0508')
-ids = es.indices.get('*')
-for i in ids:
-	print i
+
+def main():
+	es = Elasticsearch(['http://10.168.105.153'])
+	ids = es.indices.get('*')
+	for k in ids:
+		# print k, ids[k]
+		print es.count(index=k)
+
+if __name__ == '__main__':
+	main()
