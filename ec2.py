e0eedf530f67a791faecce010332331040204756
gqdw <aca_jingru@qq.com>
2016-05-08 09:47:15 +0800

    get index , add index, delete index

--- ec2.py
+++ ec2.py
@@ -5,4 +5,11 @@ from elasticsearch import Elasticsearch
 
 # 10.168.105.153:9200
 es = Elasticsearch(['http://10.168.105.153'])
-print es.info()
+# print es.info()
+# print es.indices
+# create index test-0508
+es.indices.create(index='test-0508')
+# es.indices.delete('test-0508')
+ids = es.indices.get('*')
+for i in ids:
+	print i
